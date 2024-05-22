867124a9786b2a78debf776a1b506e130f3bcbea
Gordon GIRY <giry.gordon1@gmail.com>
2024-05-22 11:10:29 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete "tasks/:id", to: "tasks#destroy"
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
 end
+
+
+#                PATCH  /restaurants/:id(.:format)             restaurants#update
+#                DELETE /restaurants/:id(.:format)             restaurants#destroy
